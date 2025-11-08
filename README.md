180c5cee5e46b9c02ae4875ebfc9af7fea10907e
gvlenaaa <gvlenaaa@mail.ru>
2025-11-08 10:07:07 +0000

    feat: добавить форму ввода

---
+++ README.md
@@ -0,0 +1,11 @@
+# Первое приложение
+
+## Подготовка
+
+[Документация](https://test-bc740.web.app/)
+
+```
+npm i -g @ionic/cli
+npm install
+ionic serve
+```
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,14 +13,21 @@ interface Item {
   standalone: false,
 })
 export class HomePage {
-  my_name:string = "Хадыженское- лучшее пиво!"
-  my_list:string[] = ["Ляляляляляляляляля", "окакокакокакокак", "оаоаоаоаоао"]
-  my_Items: Item[] = [
-    {name: "Vkusno i grustno", class: 'Pivo', cost: 5600},
-    {name: "Hochu kushat", class: 'Pizza', cost: 3000},
-    {name: "Ne pridumala", class: 'Chto-to', cost: 92387498}
-  ]
-
   constructor() {}
+  my_name: string = "Хадыженское- лучшее пиво!";
+  my_list: string[] = ["Ляляляляляляляляля", "окакокакокакокак", "оаоаоаоаоао"];
+  my_Items: Item[] = [
+    { name: "Vkusno i grustno", class: 'Pivo', cost: 5600 },
+    { name: "Hochu kushat", class: 'Pizza', cost: 3000 },
+    { name: "Ne pridumala", class: 'Chto-to', cost: 92387498 }
+  ];
+  input_one: string = ""
+  input_two: string = ""
+  input_three: number = 0
 
+  addExclamationMark(): void {
+    this.my_name += this.input_one; // Добавляем восклицательный знак к значению my_name
+    this.my_Items.push({ name: this.input_one, class: this.input_two, cost: this.input_three })
+
+  }
 }
